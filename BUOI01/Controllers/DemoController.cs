ecdf22f7bf4268e835f3eb75c6c8c906678cd4ba
Cao Thang <thang.pc1702@gmail.com>
2020-05-10 10:41:03 +0700

    Buoi 1

---
+++ BUOI01/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BUOI01.Controllers
+{
+    public class DemoController : Controller
+    {
+        public string Chuoi()
+        {
+            return "Xin Chao";
+        }
+
+        public int So()
+        {
+            return new Random().Next(10,100);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
